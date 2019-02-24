7db3a8f3bb8ef6dba23678435721f1336d77ac72
ceboulba <antoine@archipicture.fr>
2019-02-24 23:58:12 +0100

    interface

--- src/js/calc.js
+++ src/js/calc.js
@@ -3,66 +3,69 @@ function Calc() {
   <div class="hero-body">
     <div class="container">
 
-          <div class="columns is-multiline is-mobile is-vcentered">
+      <div class="columns is-multiline is-mobile is-vcentered">
+        <div class="column is-12">
+          <p class="box black">result</p>
+        </div>
 
-            <div class="column is-12">
-              <p class="box black">result</p>
-            </div>
+        <div class="column is-6">
+          <p class="box red">AC</p>
+        </div>
+        <div class="column is-3">
+          <p class="box gray has-text-centered">\/</p>
+        </div>
+        <div class="column is-3">
+          <p class="box gray has-text-centered">X</p>
+        </div>
 
-            <div class="column is-6">
-              <p class="box red">AC</p>
-            </div>
-            <div class="column is-3">
-              <p class="box gray has-text-centered">\/</p>
-            </div>
-            <div class="column is-3">
-              <p class="box gray has-text-centered">X</p>
-            </div>
+        <div class="column is-3">
+          <p class="box dark-gray has-text-centered">7</p>
+        </div>
+        <div class="column is-3">
+          <p class="box dark-gray has-text-centered">8</p>
+        </div>
+        <div class="column is-3">
+          <p class="box dark-gray has-text-centered">9</p>
+        </div>
+        <div class="column is-3">
+          <p class="box gray has-text-centered">-</p>
+        </div>
 
-            <div class="column is-3">
-              <p class="box dark-gray has-text-centered">7</p>
-            </div>
-            <div class="column is-3">
-              <p class="box dark-gray has-text-centered">8</p>
-            </div>
-            <div class="column is-3">
-              <p class="box dark-gray has-text-centered">9</p>
-            </div>
-            <div class="column is-3">
-              <p class="box gray has-text-centered">-</p>
-            </div>
+        <div class="column is-3">
+          <p class="box dark-gray has-text-centered">4</p>
+        </div>
+        <div class="column is-3">
+          <p class="box dark-gray has-text-centered">5</p>
+        </div>
+        <div class="column is-3">
+          <p class="box dark-gray has-text-centered">6</p>
+        </div>
+        <div class="column is-3">
+          <p class="box gray has-text-centered">+</p>
+        </div>
 
-            <div class="column is-3">
-              <p class="box dark-gray has-text-centered">4</p>
-            </div>
-            <div class="column is-3">
-              <p class="box dark-gray has-text-centered">5</p>
-            </div>
-            <div class="column is-3">
-              <p class="box dark-gray has-text-centered">6</p>
-            </div>
-            <div class="column is-3">
-              <p class="box gray has-text-centered">+</p>
-            </div>
+        <div class="column is-3">
+        <p class="box dark-gray has-text-centered">1</p>
+        </div>
+        <div class="column is-3">
+        <p class="box dark-gray has-text-centered">2</p>
+        </div>
+        <div class="column is-3">
+        <p class="box dark-gray has-text-centered">3</p>
+        </div>
+        <div class="column is-3">
+        <p class="box blue has-text-centered" id="equals">=</p>
+        </div>
 
-            <div class="column is-3">
-              <p class="box dark-gray has-text-centered">1</p>
-            </div>
-            <div class="column is-3">
-              <p class="box dark-gray has-text-centered">2</p>
-            </div>
-            <div class="column is-3">
-              <p class="box dark-gray has-text-centered">3</p>
-            </div>
+        <div class="column is-6">
+        <p class="box dark-gray has-text-centered">0</p>
+        </div>
+        <div class="column is-3">
+        <p class="box dark-gray has-text-centered">.</p>
+        </div>
 
-            <div class="column is-6">
-              <p class="box dark-gray has-text-centered">0</p>
-            </div>
-            <div class="column is-3">
-              <p class="box dark-gray has-text-centered">.</p>
-            </div>
 
-          </div>
+      </div>
 
     </div>
   </div>
