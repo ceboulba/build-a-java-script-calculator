ddc04f5deda41ef437b0f8f3690dc9d9a31b0549
ceboulba <antoine@archipicture.fr>
2019-02-25 15:41:38 +0100

    interfaceClean

--- src/js/calc.js
+++ src/js/calc.js
@@ -3,72 +3,25 @@ function Calc() {
   <div class="hero-body">
     <div class="container">
 
-      <div class="columns is-multiline is-mobile is-vcentered">
-
-        <div class="column is-12">
-          <p class="box black">result</p>
-        </div>
-
-        <div class="column is-6">
-          <p class="box red">AC</p>
-        </div>
-        <div class="column is-3">
-          <p class="box gray has-text-centered">\/</p>
-        </div>
-        <div class="column is-3">
-          <p class="box gray has-text-centered">X</p>
-        </div>
-
-        <div class="column is-3">
-          <p class="box dark-gray has-text-centered">7</p>
-        </div>
-        <div class="column is-3">
-          <p class="box dark-gray has-text-centered">8</p>
-        </div>
-        <div class="column is-3">
-          <p class="box dark-gray has-text-centered">9</p>
-        </div>
-        <div class="column is-3">
-          <p class="box gray has-text-centered">-</p>
-        </div>
-
-        <div class="column is-3">
-          <p class="box dark-gray has-text-centered">4</p>
-        </div>
-        <div class="column is-3">
-          <p class="box dark-gray has-text-centered">5</p>
-        </div>
-        <div class="column is-3">
-          <p class="box dark-gray has-text-centered">6</p>
-        </div>
-        <div class="column is-3">
-          <p class="box gray has-text-centered">+</p>
-        </div>
-
-        <div class="column is-3">
-        <p class="box dark-gray has-text-centered">1</p>
-        </div>
-        <div class="column is-3">
-        <p class="box dark-gray has-text-centered">2</p>
-        </div>
-        <div class="column is-3">
-        <p class="box dark-gray has-text-centered">3</p>
-        </div>
-        <div class="column is-3">
-        <p class="box blue has-text-centered" id="equals">=</p>
-        </div>
-
-        <div class="column is-6">
-        <p class="box dark-gray has-text-centered">0</p>
-        </div>
-        <div class="column is-3">
-        <p class="box dark-gray has-text-centered">.</p>
-        </div>
-
-      </div>
-
       <div class="calculatrice">
-      <div class="display box black"></div>
+        <div class="bt display">display</div>
+        <div class="bt ac">AC</div>
+        <div class="bt divi">/</div>
+        <div class="bt mult">X</div>
+        <div class="bt min">-</div>
+        <div class="bt add">+</div>
+        <div class="bt equ">=</div>
+        <div class="bt seven">7</div>
+        <div class="bt eight">8</div>
+        <div class="bt nine">9</div>
+        <div class="bt for">4</div>
+        <div class="bt five">5</div>
+        <div class="bt six">6</div>
+        <div class="bt one">1</div>
+        <div class="bt two">2</div>
+        <div class="bt three">3</div>
+        <div class="bt zero">0</div>
+        <div class="bt dec">.</div>
       </div>
 
     </div>
